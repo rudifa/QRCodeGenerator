2c5c8fef8e27e6b3425d73700a5818c82ef86844
Rudi Farkas <rudi.farkas@gmail.com>
2020-04-08 18:34:29 +0200

    add color and qrCodeGenerator

--- QRCodeGenerator/ColorMatrixViewController.swift
+++ QRCodeGenerator/ColorMatrixViewController.swift
@@ -9,6 +9,33 @@
 import UIKit
 
 class ColorMatrixViewController: UIViewController {
+    struct Color {
+        var red: CGFloat = 0.0
+        var green: CGFloat = 0.0
+        var blue: CGFloat = 0.0
+        var alpha: CGFloat = 1.0
+
+        var ciColor: CIColor {
+            return CIColor(red: red, green: green, blue: blue, alpha: alpha)
+        }
+    }
+
+    var qrCodeGenerator = QRCodeGenerator()
+
+    var color = Color() {
+        didSet {
+            updateImages()
+        }
+    }
+
+    private func updateImages() {
+        qrCodeGenerator.foregroundColor = color.ciColor
+        qrCodeGenerator.mode = .coloredOnWhite
+        baseColorImageView.image = qrCodeGenerator.uiImage
+        qrCodeGenerator.mode = .coloredOnClear
+        transformedColorImageView.image = qrCodeGenerator.uiImage
+    }
+
     private lazy var baseColorImageView: UIImageView = {
         let view = UIImageView()
         view.backgroundColor = .lightGray
@@ -28,8 +55,9 @@ class ColorMatrixViewController: UIViewController {
     private lazy var redColorSlider: UISlider = {
         let slider = UISlider()
         slider.translatesAutoresizingMaskIntoConstraints = false
-        slider.maximumValue = 255
+        slider.maximumValue = 1.0
         slider.minimumValue = 0
+        slider.value = 0.5
         slider.isContinuous = true
         slider.addTarget(self, action: #selector(redColorSliderValueChanged), for: .touchDragInside)
         return slider
@@ -38,8 +66,9 @@ class ColorMatrixViewController: UIViewController {
     private lazy var greenColorSlider: UISlider = {
         let slider = UISlider()
         slider.translatesAutoresizingMaskIntoConstraints = false
-        slider.maximumValue = 255
+        slider.maximumValue = 1.0
         slider.minimumValue = 0
+        slider.value = 0.5
         slider.isContinuous = true
         slider.addTarget(self, action: #selector(greenColorSliderValueChanged), for: .touchDragInside)
         return slider
@@ -48,8 +77,9 @@ class ColorMatrixViewController: UIViewController {
     private lazy var blueColorSlider: UISlider = {
         let slider = UISlider()
         slider.translatesAutoresizingMaskIntoConstraints = false
-        slider.maximumValue = 255
+        slider.maximumValue = 1.0
         slider.minimumValue = 0
+        slider.value = 0.5
         slider.isContinuous = true
         slider.addTarget(self, action: #selector(blueColorSliderValueChanged), for: .touchDragInside)
         return slider
@@ -57,16 +87,18 @@ class ColorMatrixViewController: UIViewController {
 
     @objc func redColorSliderValueChanged() {
         printClassAndFunc(info: "value= \(redColorSlider.value)")
+        color.red = CGFloat(redColorSlider.value)
     }
 
     @objc func greenColorSliderValueChanged() {
         printClassAndFunc(info: "value= \(greenColorSlider.value)")
+        color.green = CGFloat(greenColorSlider.value)
     }
 
     @objc func blueColorSliderValueChanged() {
         printClassAndFunc(info: "value= \(blueColorSlider.value)")
+        color.blue = CGFloat(blueColorSlider.value)
     }
-
 
     private lazy var colorImageHStack = UIStackView.horizontal(subviews: [baseColorImageView, transformedColorImageView])
 
@@ -77,7 +109,9 @@ class ColorMatrixViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .black
+        colorImageHStack.distribution = .fillEqually
+
+        view.backgroundColor = .gray
         view.addSubview(mainVStack)
         layoutSubviews()
 
@@ -86,15 +120,31 @@ class ColorMatrixViewController: UIViewController {
         view.addGestureRecognizer(tap)
     }
 
-    func layoutSubviews() {
-         let safeAreaLayoutGuide = view.safeAreaLayoutGuide
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        qrCodeGenerator.imageSidePt = view.frame.size.width * 0.4
 
-         NSLayoutConstraint.activate([
-             mainVStack.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
-             mainVStack.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
-             mainVStack.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: 8),
-         ])
-     }
+        // CIColor(red: 0.206, green: 0.599, blue: 0.860) // skyBlue
+        redColorSlider.value = 0.206
+        greenColorSlider.value = 0.599
+        blueColorSlider.value = 0.860
+
+        color.red = CGFloat(redColorSlider.value)
+        color.green = CGFloat(greenColorSlider.value)
+        color.blue = CGFloat(blueColorSlider.value)
+
+        updateImages()
+    }
+
+    func layoutSubviews() {
+        let safeAreaLayoutGuide = view.safeAreaLayoutGuide
+
+        NSLayoutConstraint.activate([
+            mainVStack.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            mainVStack.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            mainVStack.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: 8),
+        ])
+    }
 
     @objc func doubleTapped() {
         backToViewController()
